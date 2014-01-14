8a1571e29247449562dda927ab5bbd7804fb70ed
Tom Bujok <tom.bujok@gmail.com>
2014-01-14 20:05:42 +0100

    Rename ape.pl to xvm.pl

--- ape.pl
+++
@@ -1,4 +0,0 @@
-use strict;
-use warnings;
-
-print "Hello ape - running perl scripts on windows!\n";
---
+++ xvm.pl
@@ -0,0 +1,4 @@
+use strict;
+use warnings;
+
+print "Hello ape - running perl scripts on windows!\n";
